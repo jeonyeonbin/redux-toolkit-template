195e97cd0c8819d4594280773e916123d647cb13
JeonYeonBIn <jyb5858@nts-corp.com>
2020-10-01 18:35:05 +0900

    feat: convert Redux to reduxtoolkit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
-import rootReducer from "../ducks";
-const store = createStore(rootReducer);
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
+import rootReducer from "$Root/ducks";
+const store = configureStore({ reducer: rootReducer });
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
