a5257520e83d9f9d2c6eedecef7863dabf12c089
JeonYeonBIn <jyb5858@nts-corp.com>
2020-10-01 18:48:13 +0900

    feat: redux logger settings

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-
+import logger from "redux-logger";
 import rootReducer from "$Root/ducks";
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== "production",
+});
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
