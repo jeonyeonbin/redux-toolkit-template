500873544fb3efdea5f28d7a7cf05eb12ed26c25
JeonYeonBIn <qtrgor@gmail.com>
2020-10-05 15:57:54 +0900

    feat: setup scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import Test from "$Components/Test";
+import SassComponent from "$Components/SassComponent";
 
 function App() {
-  return <Test />;
+  return (
+    <>
+      <Test /> <SassComponent />
+    </>
+  );
 }
 
 export default App;
