e07e8b20bb1a6419c3e41ad6476b0ff57e36700e
JeonYeonBIn <qtrgor@gmail.com>
2020-10-07 23:34:25 +0900

    feat: axios caching default

--- src/util/customAxios.ts
+++ src/util/customAxios.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
+import { setupCache } from "axios-cache-adapter";
+const cache = setupCache({
+  debug: process.env.NODE_ENV !== "production",
+  maxAge: 15 * 60 * 1000,
+});
+
 const instance = axios.create({
   baseURL: process.env.REACT_APP_GITHUB_API_URL,
   headers: {
     Authorization: process.env.REACT_APP_GITHUB_TOKEN,
   },
+  adapter: cache.adapter,
   timeout: 2500,
 });
 
