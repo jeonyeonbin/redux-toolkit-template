26354c79392a44550d71630e3b7b1c4dd730447b
JeonYeonBIn <qtrgor@gmail.com>
2020-10-09 12:11:05 +0900

    feat: page append

---
+++ src/pages/counter/CounterPage.tsx
@@ -0,0 +1,11 @@
+import Counter from "$Components/Counter";
+import React from "react";
+
+function CounterPage({}) {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+export default CounterPage;
---
+++ src/pages/todo/TodoPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import useTodos from "$Root/hooks/useTodos";
+import TodoInput from "$Components/TodoInput";
+import TodoItem from "$Components/TodoItem";
+
+function TodoPage() {
+  const { todos } = useTodos();
+  return (
+    <>
+      <TodoInput />
+      {todos?.map(({ id, text }, index) => (
+        <TodoItem key={index} {...{ id, text }} />
+      ))}
+    </>
+  );
+}
+
+export default TodoPage;
