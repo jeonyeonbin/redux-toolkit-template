279a2f6c0e8b2d6357d2e98652ffdaca3dbffdba
jaehyeok.jeong <jh.jeong@nts-corp.com>
2020-10-26 11:07:34 +0900

    fix setting
    - add @types/react-redux
    - fix eslint same key settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,12 +40,9 @@ module.exports = {
       },
       // use <root>/tsconfig.json
       "typescript": {
-        "alwaysTryTypes": true // always try to resolve types under `<root>@types` directory even it doesn"t contain any source code, like `@types/unist`
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn"t contain any source code, like `@types/unist`
+        "project": "./tsconfig.json" // use <root>/path/to/folder/tsconfig.json
       },
-      // use <root>/path/to/folder/tsconfig.json
-      "typescript": {
-        "project": "./tsconfig.json"
-      }
     }
   },
 }
